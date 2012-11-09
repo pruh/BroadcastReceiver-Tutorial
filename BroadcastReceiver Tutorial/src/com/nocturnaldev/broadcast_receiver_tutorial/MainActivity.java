289e9aeba62ed12f2e9f68d8c5d74699a9b452c0
Ivan Davletshin <j.rooft@gmail.com>
2012-11-09 15:25:01 +0400

    Initial source code commit

---
+++ BroadcastReceiver Tutorial/src/com/nocturnaldev/broadcast_receiver_tutorial/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nocturnaldev.broadcast_receiver_tutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
